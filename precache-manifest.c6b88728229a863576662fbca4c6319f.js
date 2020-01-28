fd76abc86718729fbe6e36cc16d8e2361f0456d7
boandriy <borovec@ucu.edu.ua>
2020-01-28 10:45:08 +0200

    Updates

---
+++ precache-manifest.c6b88728229a863576662fbca4c6319f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd7fdd07924d5341502e313d538af31b",
+    "url": "/todo-react-typescript/index.html"
+  },
+  {
+    "revision": "a14be9a69985a33ae205",
+    "url": "/todo-react-typescript/static/css/main.4ed4acb1.chunk.css"
+  },
+  {
+    "revision": "1b377705415e136929f2",
+    "url": "/todo-react-typescript/static/js/2.a4160df3.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/todo-react-typescript/static/js/2.a4160df3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a14be9a69985a33ae205",
+    "url": "/todo-react-typescript/static/js/main.b2aa9869.chunk.js"
+  },
+  {
+    "revision": "ca8c11831a08df8f5724",
+    "url": "/todo-react-typescript/static/js/runtime-main.466567b5.js"
+  }
+]);
